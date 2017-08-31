20a51a41fe0fc665d11ff6b3e827dc3a934d88d5
PyPiPie <kirlewraheem@gmail.com>
2017-08-30 21:29:42 -0500

    Update RDL.py

--- RDL.py
+++ RDL.py
@@ -5,7 +5,7 @@ ydl_opts = {}
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                                            video=raw_input('Enter video url:')
 ydl.download([video])
-source_file='C:\Users\Donna\Desktop\Python'
-dest_folder='C:\Users\Donna\Desktop'
+source_file='C:\Users\(username)\Desktop\Python'
+dest_folder='C:\Users\(username)\Desktop'
 shutil.move(source_file,dest_folder)
 print 'Check desktop for the file created'
